4748f089e76ec8ae421b4593e44ca97369eccb40
apetitescure <85885961+apetitescure@users.noreply.github.com>
2024-01-09 22:52:54 -0500

    format main.glsl

--- ascii/main.glsl
+++ ascii/main.glsl
@@ -1,16 +1,16 @@
 //---------------------------------------------------------------------------
 float character(int n, vec2 p)
 {
-	p = floor(p*vec2(-4.0, 4.0) + 2.5);
+    p = floor(p*vec2(-4.0, 4.0) + 2.5);
     if (clamp(p.x, 0.0, 4.0) == p.x)
-	{
+    {
         if (clamp(p.y, 0.0, 4.0) == p.y)
-		{
-        	int a = int(round(p.x) + 5.0 * round(p.y));
-			if (((n >> a) & 1) == 1) return 1.0;
-		}
+        {
+            int a = int(round(p.x) + 5.0 * round(p.y));
+            if (((n >> a) & 1) == 1) return 1.0;
+        }
     }
-	return 0.0;
+    return 0.0;
 }
 
 //---------------------------------------------------------------------------
@@ -22,12 +22,12 @@ vec4 asciiCharacter(in vec2 pix, in vec4 col)
 
     // limited character set
     if (gray > 0.2) n = 65600;    // :
-	if (gray > 0.3) n = 163153;   // *
-	if (gray > 0.4) n = 15255086; // o
-	if (gray > 0.5) n = 13121101; // &
-	if (gray > 0.6) n = 15252014; // 8
-	if (gray > 0.7) n = 13195790; // @
-	if (gray > 0.8) n = 11512810; // #
+    if (gray > 0.3) n = 163153;   // *
+    if (gray > 0.4) n = 15255086; // o
+    if (gray > 0.5) n = 13121101; // &
+    if (gray > 0.6) n = 15252014; // 8
+    if (gray > 0.7) n = 13195790; // @
+    if (gray > 0.8) n = 11512810; // #
 
     // full character set including A-Z and 0-9
 
@@ -74,9 +74,9 @@ vec4 asciiCharacter(in vec2 pix, in vec4 col)
     if (gray > 0.9535) n = 33061407;
     if (gray > 0.9767) n = 11512810;
 
-	vec2 p = mod(pix/4.0, 2.0) - vec2(1.0);
+    vec2 p = mod(pix/4.0, 2.0) - vec2(1.0);
 
-	col = col*character(n, p);
+    col = col*character(n, p);
 
-	return vec4(col.xyz, 1.0);
+    return vec4(col.xyz, 1.0);
 }
